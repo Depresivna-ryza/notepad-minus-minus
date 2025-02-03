26255ef2ca65c05a45ca8792df14392bd253d1d6
Mário Hatalčík <xhatalc@fi.muni.cz>
2025-02-03 14:15:18 +0100

    fix dividers

--- src/views/sessionexplorer.rs
+++ src/views/sessionexplorer.rs
@@ -5,7 +5,7 @@ pub fn SessionsExplorer() -> Element {
     rsx! {
         div {
             style: "display: flex; flex: 1; background-color: rgb(53, 40, 40);  font-family: JetBrains Mono; font-size: 20px; color: white; 
-                    justify-content: center; align-items: center;",
+                    justify-content: center; align-items: center; width: 100%;",
                 "Sessions"
         }
     }
