b42b08a36a84618ec470641cde6ff93ce196e16e
Richard Schwarz <rs.richard.schwarz@gmail.com>
2025-02-03 10:56:32 +0100

    feat: network now listens on different ports for host and listener, handling of different SwarmEvents

--- src/models/network.rs
+++ src/models/network.rs
@@ -1,51 +1,25 @@
 use libp2p::{
-    Multiaddr,
-    futures::StreamExt,
     floodsub::{Floodsub, FloodsubEvent, Topic},
-    identity, noise, swarm::{Swarm, SwarmEvent}, tcp, yamux, PeerId, SwarmBuilder
+    futures::StreamExt,
+    identity, noise,
+    swarm::{Swarm, SwarmEvent},
+    tcp, yamux, Multiaddr, PeerId, SwarmBuilder,
 };
-use tokio::sync::mpsc;
+use std::{collections::HashSet, error::Error};
 use tracing::info;
-use std::{error::Error, collections::HashSet};
-use serde::{Serialize, Deserialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum NetworkMessage {
-    SessionJoinRequest {
-        session_id: String,
-        peer_id: String,
-    },
-    SessionJoinResponse {
-        accepted: bool,
-        session_info: super::sessions::SessionInfo,
-        file_content: Option<String>,
-    },
-    SessionContent {
-        session_id: String,
-        content: String,
-        sender: String,
-    },
-    SessionLeave {
-        session_id: String,
-        peer_id: String,
-    },
-}
-
 
 /// P2PNetwork handles libp2p networking logic.
+
 pub struct P2PNetwork {
     local_peer_id: PeerId,
-    topic: Topic,
-    swarm: Swarm<Floodsub>,
+    pub swarm: Swarm<Floodsub>,
     discovered_peers: HashSet<PeerId>,
 }
 
 impl P2PNetwork {
-    pub async fn new() -> Result<(Self, mpsc::UnboundedReceiver<NetworkMessage>), Box<dyn Error>> {
+    pub fn new(host: bool) -> Result<Self, Box<dyn Error>> {
         let local_key = identity::Keypair::generate_ed25519();
         let local_peer_id = PeerId::from(local_key.public());
-        
-        let topic = Topic::new("notepad-minus-minus");
 
         info!("Local peer id: {local_peer_id}");
 
@@ -53,52 +27,85 @@ impl P2PNetwork {
         let behaviour = Floodsub::new(local_peer_id);
 
         let mut swarm = SwarmBuilder::with_existing_identity(local_key)
-        .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new,
-            yamux::Config::default,
-        )?
-        .with_behaviour(|_| behaviour)?
-        .build();
+            .with_tokio()
+            .with_tcp(
+                tcp::Config::default(),
+                noise::Config::new,
+                yamux::Config::default,
+            )?
+            .with_behaviour(|_| behaviour)?
+            .build();
+
         // Listen on all interfaces
-        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-        
+        if host {
+            swarm.listen_on("/ip4/0.0.0.0/tcp/5000".parse()?)?;
+        } else {
+            swarm.listen_on("/ip4/0.0.0.0/tcp/5666".parse()?)?;
+        }
+
         // Create a channel for receiving messages
-        let (message_sender, message_receiver) = mpsc::unbounded_channel();
-        
+        // Do this when creating a session not entire network
+        // let (message_sender, message_receiver) = mpsc::unbounded_channel();
+
         let network = Self {
             local_peer_id,
-            topic,
             swarm,
             discovered_peers: HashSet::new(),
         };
 
-        Ok((network, message_receiver))
+        Ok(network)
     }
 
-    pub async fn send_data(&mut self,data: String, topic: Topic){
+    pub async fn send_data(&mut self, data: String, topic: Topic) {
         self.swarm.behaviour_mut().publish_any(topic, data);
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
         loop {
+            self.swarm
+                .behaviour_mut()
+                .publish(Topic::new("notepad"), "Chytim mikrofon");
             match self.swarm.select_next_some().await {
+                SwarmEvent::Dialing {
+                    peer_id,
+                    connection_id,
+                } => {
+                    print!("Dialed by peer {:?}", peer_id);
+                }
+                SwarmEvent::IncomingConnection {
+                    connection_id,
+                    local_addr,
+                    send_back_addr,
+                } => {
+                    println!("Incoming ")
+                }
+                SwarmEvent::ConnectionEstablished {
+                    peer_id,
+                    connection_id,
+                    endpoint,
+                    num_established,
+                    concurrent_dial_errors,
+                    established_in,
+                } => {
+                    println!("Connection came from {} ", peer_id);
+                }
                 SwarmEvent::Behaviour(FloodsubEvent::Message(message)) => {
                     println!("Received message: {:?}", message);
                 }
-                SwarmEvent::Behaviour(FloodsubEvent::Subscribed{peer_id, topic}) => {
+                SwarmEvent::Behaviour(FloodsubEvent::Subscribed { peer_id, topic }) => {
                     self.discovered_peers.insert(peer_id);
                     println!("Connected to peer: {peer_id}");
                 }
-                SwarmEvent::Behaviour(FloodsubEvent::Unsubscribed{peer_id, topic}) => {
+                SwarmEvent::Behaviour(FloodsubEvent::Unsubscribed { peer_id, topic }) => {
                     self.discovered_peers.remove(&peer_id);
                     println!("Disconnected from peer: {peer_id}");
                 }
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Listening on {address:?}");
                 }
-                _ => {}
+                e => {
+                    println!("Received fuck all message: {:?}", e);
+                }
             }
         }
     }
